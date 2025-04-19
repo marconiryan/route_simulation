da405c6cf100f742624dacb31d6b16126132a4a6
marconiryan <tencatenryan11@gmail.com>
2025-04-19 10:19:16 -0300

    Add configuration file parsing for router and link setups
    Introduced `roteador.config` and `enlaces.config` files to define router and link configurations. Updated `main.c` to parse and display configuration details, including router IDs, ports, IPs, and link costs, based on the provided router ID argument.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,69 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+#define ROUTER_CONFIG_PATH "../roteador.config"
+#define LINKS_CONFIG_PATH "../enlaces.config"
+
+const char *ID;
+
+void line_separator() {
+    printf("----------------------------------------\n");
+}
+
+void read_router_config() {
+    FILE *file = fopen(ROUTER_CONFIG_PATH, "r");
+    if (!file) {
+        fprintf(stderr, "Error opening '%s' file\n", ROUTER_CONFIG_PATH);
+        return;
+    }
+
+    int id, port;
+    char ip[16];
+    int found = 0;
+
+    while (fscanf(file, "%d %d %15s", &id, &port, ip) == 3) {
+        printf("Router ID: %d\n", id);
+        printf("Port: %d\n", port);
+        printf("IP: %s\n", ip);
+        line_separator();
+        found++;
+    }
+
+    fclose(file);
+}
+
+void read_links_config() {
+    FILE *file = fopen(LINKS_CONFIG_PATH, "r");
+    if (!file) {
+        fprintf(stderr, "Error opening '%s' file\n", LINKS_CONFIG_PATH);
+        return;
+    }
+
+    int id_from, id_to, cost;
+    int found = 0;
+
+    while (fscanf(file, "%d %d %d", &id_from, &id_to, &cost) == 3) {
+        line_separator();
+        printf("ID from: %d\n", id_from);
+        printf("ID to: %d\n", id_to);
+        printf("Cost: %d \n", cost);
+        found++;
+    }
+
+    fclose(file);
+}
+
+int main(const int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Error: Please provide an ID as an argument.\n");
+        return 1;
+    }
+
+    ID = argv[1];
+
+    printf("Hello, Router ID: %s\n\n", ID);
+    line_separator();
+    read_router_config();
+    read_links_config();
+
     return 0;
 }
